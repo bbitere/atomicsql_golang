3bf062022b2e760e76ec5cd3feb1bd325063af25
bogdan.bitere <bogdan.bitere@newtown.ro>
2023-06-07 12:33:24 +0300

    poop

---
+++ create_repo_atomicsql.bat
@@ -0,0 +1,10 @@
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/bbitere/atomicsql_golang.git
+git push -u origin main
+pause
+
+
