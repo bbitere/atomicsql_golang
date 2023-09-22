07f32f6a0bf841ff382a412b4cb9434f5e810a8d
bogdan.bitere <bogdan.bitere@newtown.ro>
2023-09-22 13:46:18 +0300

    upd

--- library/src/go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/bbitere/atomicsql_golang.git
-
-go 1.20
-
-require (
-	//github.com/georgysavva/scany/v2 v2.0.0
-)
--- library/src/tests/test1/sql_def/generateDB.go
+++ library/src/tests/test1/sql_def/generateDB.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	importer "go/importer"
+	orm "github.com/bbitere/atomicsql_golang/atomicsql"
 )
 
 func main(){
