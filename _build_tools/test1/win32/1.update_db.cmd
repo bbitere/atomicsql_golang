295a66242790948d328a6e43e449bdce3c40d886
bogdan.bitere <bogdan.bitere@newtown.ro>
2023-09-26 17:23:54 +0300

    upd

--- _build_tools/test1/win32/1.update_db.cmd
+++ _build_tools/test1/win32/1.update_db.cmd
@@ -1,21 +1,29 @@
 call 0.common_config.cmd
 
 @echo ------------------------------------------------------
+@echo "1. parse golang models -> generate_jsons"
 %GoScan% -o -i -h -e=\.gen\.go -z=%CD%\asqlmodels-config.cfg   %SRCDIR_PATH%\tests\test1\mymodels
 
 
 @echo ------------------------------------------------------
+@echo "2. parse generated_jsons -> generate sql_scripts"
 %GoServerTool% -asql_migration  -sql_lang=postgres -execute_scripts=n -json_dir=%SRCDIR_PATH%\_db_migration -delimeter=@@@@@@@@######@@@@@@^
 	   -connection_string=%CONNECTION_STRING%
 
 @echo ------------------------------------------------------
+@echo "3. apply sql_scripts in database, iterative and only once."
 %GoServerTool% -migration_db  -sql_lang=postgres -input_dir="%SRCDIR_PATH%\_db_migration"^
  -out_dir="" -type_out_file=go^
  -connectionString=%CONNECTION_STRING%
   
 @echo ------------------------------------------------------ 
- rem create the models from Database
- %GoServerTool% -export_db -config_file="%CD%\cfg_exportdb.cfg" -type_out_file=go
- pause
+@echo "4. create the additional golang models information from database, useful for atomicsql engine"
+%GoServerTool% -export_db -config_file="%CD%\cfg_exportdb.cfg" -type_out_file=go
+pause
+
+@echo ------------------------------------------------------ 
+@echo "5. generate the sql seqs for lambda expression : where() & select()"
+%GoScan% -o -i -h -q=.\sql-config.cfg %SRCDIR_PATH%\tests\test1\test_where
+pause
  
 
--- _build_tools/test1/win32/4.scan_queries.cmd
+++ _build_tools/test1/win32/4.scan_queries.cmd
@@ -1,11 +1,5 @@
-rem cd ..
+call 0.common_config.cmd
 
-@echo off
-set DIR_TOOL=..\..\..\server-tools\atomicsql_compiler_tool\go2cs\go2cs-master\src\go2cs\bin\SqlConv\net7.0
-set path=%path%;%CD%\%DIR_TOOL%;
 
-SET SVCTOOL=%DIR_TOOL%\go2cs.exe
-
-rem merge dar doar full path,pt ca directorul curent il modifica in C:\users
-%SVCTOOL% -o -i -h -q=.\sql-config.cfg C:\Work\SlotMonitor\SlotGames\server-main\NewtonGamesGo\pkg\controllers 
+%GoScan% -o -i -h -q=.\sql-config.cfg %SRCDIR_PATH%\tests\test1\test_where
 pause
