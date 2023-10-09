225100d9bf0d6990af897409c4b660e1288c7367
bogdan.bitere <bogdan.bitere@newtown.ro>
2023-10-09 09:45:48 +0300

    upd

--- _build_tools/test1/win32/0.common_config_template.cmd
+++ _build_tools/test1/win32/0.common_config_template.cmd
@@ -10,6 +10,6 @@ set GoScan=%GoScanPath%\go2cs.exe
 set GoServerTool=%GoServerToolPath%\DBTool.exe
 
 set SRCDIR_PATH=%CD%\..\..\..\library
-set CONNECTION_STRING=Host=localhost;Username=dev_original;Password=XCZ12345678;Database=test1
+set CONNECTION_STRING=Host=localhost;Username=??;Password=??;Database=test1
 set GITHUB_URL=github.com/bbitere/atomicsql_golang.git
 rem set GITHUB_URL="github.com/bbitere/atomicsql_golang.git/src/atomicsql"
