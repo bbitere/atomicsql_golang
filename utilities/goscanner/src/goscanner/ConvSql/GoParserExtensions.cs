98580211e155a5cc305117e2dd11e327f2e45674
bogdan.bitere <bogdan.bitere@newtown.ro>
2023-12-22 20:02:32 +0200

    add extenstions

---
+++ utilities/goscanner/src/goscanner/ConvSql/GoParserExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+
+public partial class GoParser{
+    public partial class PrimaryExprContext {
+
+        public string m_funcMethodName=null;
+        public string m_classMethod;
+    }
+}
+
+namespace Antlr4.Runtime
+{
+    
+
+}
+
